9ca7ee6375096635deda6c563f1049998e5ea6c6
Jaki <143355050+jaki-boi@users.noreply.github.com>
2024-12-08 14:31:14 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import human
+import cat
+
+#Made by [NULL]
+#This code is the primary software that runs in femboys
+
+
+skirt = true
+thighHighs = true
+
+def getSexuality(kisser):
+  if gay == true:
+   status = "boykisser"
+  
+  elif bi == true:
+   status = "allkisser"
+  
+  elif straight == true:
+    status = "girlkisser"
+
+  else:
+    status = "nothingkisser"
+    
+  return(status)
+
+def inputMonster():
+
+  if caffine <= 8:
+    print("warn: not enough monster energy")
+
+  elif caffine >= 10:
+    print("warn: excessive monster energy"")
+
+  else:
+    print("monster intake normal")
+
+  
